1db0caf80d1ea0f9d61ec42f7e2b36938bed24b5
Kato-Souma <20010503@anabuki-net.ne.jp>
2021-06-15 12:34:37 +0900

    無視リストに入れていたインクルードファイルを追加し直しました。

--- ゲーム制作プログラムファイル/GameProgramming/CGoal.h
+++ ゲーム制作プログラムファイル/GameProgramming/CGoal.h
@@ -3,12 +3,15 @@
 
 #include "CCharacter.h"
 
+#include "CCollider.h"
+
 class CGoal :public CCharacter{
 public:
 	//コンストラクタ
 	//CGoal(モデル,位置,回転,拡縮)
 	CGoal(CModel*model, CVector position, CVector rotation, CVector scale);
 
+	CCollider mCollider1;
 	
 };
 
